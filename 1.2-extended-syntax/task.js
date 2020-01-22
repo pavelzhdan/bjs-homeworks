16756a7a335ec760516d31d9b2919210dda588d1
Pavel Zhdanovich <zhdan21@gmail.com>
2020-01-22 20:05:29 +0300

    Debugger added and second homework complete

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function calculateQuadraticEquation(){
     let a = +window.a.value;
     let b = +window.b.value;
@@ -9,9 +11,25 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
-}
+    let result = [];
+
+    let d = Math.pow(b,2)-4*a*c;
+    let x1 = (-b+Math.sqrt(d))/2*a;
+    let x2 = (-b-Math.sqrt(d))/2*a;
+
+    switch(true) {
+        case d<0:
+            result = [];
+            break;
+        case d===0:
+            result = [x1];
+            break;
+        default:
+            result = [x1, x2];
+    };
+
+    return result;
+};
 
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
@@ -20,8 +38,20 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+    if (marks.length > 5){
+        alert (`Вы ввели более пяти оценок! Всего оценок ${marks.length}, посление(яя) ${marks.length-5} будут(ет) удалены(а)`);
+        marks.splice(5,marks.length-5);
+      };
+      
+    let marksSum = 0;
+      
+    for (let i=0; i<marks.length; i++){
+        marksSum = marksSum + marks[i];
+    };
+      
+    let averageMark = marksSum / marks.length;
+    
+    return averageMark;
 }
 
 function calculateDrinkTask(){
@@ -32,7 +62,21 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let yearOfBirth = dateOfBirthday.getFullYear();
+    let today = new Date();
+    let thisYear = today.getFullYear();
+
+    let age = thisYear-yearOfBirth;
+    let result;
+
+    switch(true){
+        case age < 18:
+            result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+            break;
+        case age > 18:
+            result = `Не желаете ли олд-фэшн, ${name}?`;
+            break;
+        }
+    console.log(result);
+    return result;
 }
