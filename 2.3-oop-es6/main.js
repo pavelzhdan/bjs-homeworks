2be64bef5cc2cc434aae2c293436c78f529c018e
Pavel Zhdanovich <zhdan21@gmail.com>
2020-02-06 21:02:27 +0300

    Homework 6 amendment

--- 2.3-oop-es6/main.js
+++ 2.3-oop-es6/main.js
@@ -40,29 +40,9 @@ const sword = new Weapon("Меч", 25, 500, 1);
 const knife = new Weapon("Нож", 5, 300, 1);
 const staff = new Weapon("Посох", 8, 300, 2);
 
-/*class EnforcedWeapon extends Weapon{
-    constructor(name, originName, attack, range, durability){
-        super(originName, attack, range, durability);
-        this.name = name;
-        this.originName = originName;
-        switch(true){
-            case originName == "Лук":
-                this.durability = bow._durability;
-                break;
-            case originName == "Меч":
-                this.range = sword.range;
-                break;
-            case originName == "Посох":
-                this.durability = staff._durability;
-                break;
-        };
-    };
-};
-
-
-const longbow = new EnforcedWeapon("Длинный лук", "Лук", 15, 4, "-");
-const poleaxe = new EnforcedWeapon("Секира", "Меч", 27,"-", 800);
-const stormStaff = new EnforcedWeapon("Посох Бури", "Посох", 10, 3, "-")*/
+const longbow = new Weapon("Длинный лук", 15, 200, 4);
+const poleaxe = new Weapon("Секира", 27, 800, 1);
+const stormStaff = new Weapon("Посох Бури", 10, 300, 3);
 
 class Hand extends Weapon{
     constructor(){
@@ -91,5 +71,106 @@ class Knife extends Weapon{
 class Staff extends Weapon{
     constructor(){
     super("Посох", 8, 300, 2);
+    };
+};
+
+class Longbow extends Bow{
+    constructor(){
+        super();
+        this.name = "Длинный лук";
+        this.attack = 15;
+        this.range = 4;
+    };
+};
+
+class Poleaxe extends Sword{
+    constructor(){
+        super();
+        this.name = "Секира";
+        this.attack = 27;
+        this.durability = 800;
+        this._durability = 800;
+    };
+};
+
+class StormStaff extends Staff{
+    constructor(){
+        super();
+        this.name = "Посох Бури";
+        this.attack = 10;
+        this.range = 3;
+    };
+};
+
+const hand1 = new Hand();
+const bow1 = new Bow();
+const sword1 = new Sword();
+const knife1 = new Knife();
+const staff1 = new Staff();
+
+const longbow1 = new Longbow();
+const poleaxe1 = new Poleaxe();
+const stormStaff1 = new StormStaff();
+
+
+
+//Задача 3
+
+class StudentLog{
+    constructor(studentName){
+        this.studentName = studentName;
+    };
+
+    get getName(){
+        return this.studentName
+    };
+
+    addGrade(grade, subject){
+        if (grade > 5 || grade < 0 || typeof(grade) == "string"){
+                alert(`Вы пытались поставить оценку ${grade} по предмету ${subject}. Допускаются только числа от 1 до 5.`);
+                if (!this[subject]){
+                    return 0
+                } else {
+                    return this[subject].length
+                };
+            } if (this.hasOwnProperty(subject)) {
+                this[subject][this[subject].length] = grade;   
+                return this[subject].length
+            } else {
+            this[subject] = [grade];
+            return this[subject].length
+        };
+    };
+
+    getAverageBySubject(subject){
+        if (!this[subject]){
+            return 0
+        } else{
+            let averageGrade = 0;
+            for (let i = 0; i < this[subject].length; i++){
+                averageGrade += this[subject][i];
+            };
+            return averageGrade / this[subject].length;
+        };
+    };
+
+    get getTotalAverage(){
+        let subjectList = Object.getOwnPropertyNames(this);
+        subjectList.splice(0,2);
+        let marksQuntity = 0;
+        let gradeSumm = 0;
+        for (let l=0; l<subjectList.length; l++){
+            let currentSubject = String(this.subjectList[l]);
+            marksQuntity += Number(this[currentSubject].length);
+            for(let i=0; i<this[currentSubject].length; i++){
+                let subject = this[currentSubject].length;
+                gradeSumm += subject[i];
+            };
+        };
+        if(marksQuntity === 0){
+            return 0
+        } else {
+            return gradeSumm / marksQuntity
+        };
     };
 };
